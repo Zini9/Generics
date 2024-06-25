f12d05f61566f00c4044dc80592da01566ca00b2
Guilherme Zini <guilhermerosazini@gmail.com>
2024-06-25 20:17:35 -0300

    Aula Generics 1

---
+++ Generics/PrintService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class PrintService
+    {
+        //Variavel interna que poderá armazenar 10 numeros inteiros
+        private int[] _values = new int[10];
+        //Variavel pra contar quantos inteiros
+        private int _count = 0;
+
+
+        public void AddValue(int value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrinService is full");
+            }
+
+            _values[_count] = value;
+            _count++;
+        }
+
+
+        public int Firt()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrinService is empty");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for(int i = 0; i < _count -1; i++) 
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count -1]);
+            }
+
+            Console.WriteLine("]");
+        }
+
+
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando classe
+            PrintService printService = new PrintService();
+
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) 
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+            // Imprimir todos elementos
+            printService.Print();
+
+            Console.WriteLine("Firt: " + printService.Firt());
+
+        }
+    }
+}
